ec3fa49b68c80b10c966d4d5c36bdec8b445d975
kgient <kgient@naver.com>
2017-02-17 01:32:24 +0900

    Edit info

--- application/views/navbar.php
+++ application/views/navbar.php
@@ -27,19 +27,20 @@
 <div class="container-fluid">
     <div class="row">
         <div class="col-xs-6 col-md-2" style="background: white; min-height:100vh; hight:auto">
-            <table border="0" cellpadding="0" cellspacing="0" width="40"border="1">
-                <tr>
-                    <td>
-                        <img src="https://cdn0.iconfinder.com/data/icons/connection/512/icon-16.png" width="40" height="40"></td>
-                    </td>
-                    <td style = "text-align:left; vertical-align:center; padding:8px">
+            <br>
+                  <center>
+                        <img src="https://cdn0.iconfinder.com/data/icons/connection/512/icon-16.png" width="100" height="100"></td>
+                    </center>
+                <br>               
+                 <center>
+                    
+                      <b1 class="profile_style">
                         김성훈 <a href="/index.php/authentication/mypage" data-toggle="tooltip" data-placement="right" title="개인정보 수정"
                                 style="color:black;"><span class="glyphicon glyphicon-cog" aria-hidden="true"></span></a>
-                        <br>
+                        
+                        <b1 class="profile_style">
                         kgient@naver.com
-                    </td>
-                </tr>
-            </table>
-
+                    </center>
+                    
         </div>
         <div class="col-xs-12 col-md-10" style="background: #eeeeee; min-height:100vh; hight:auto">
